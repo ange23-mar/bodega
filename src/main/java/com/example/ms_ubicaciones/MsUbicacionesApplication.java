36b0d3ed166ede37de72d8628daa01419e282045
Angelica Araya <ange.arayav@duocuc.cl>
2026-06-29 00:14:14 -0400

    Correccion de yaml y ms-ubiapp en  Bodega

--- src/main/java/com/example/ms_ubicaciones/MsUbicacionesApplication.java
+++ src/main/java/com/example/ms_ubicaciones/MsUbicacionesApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MsUbicacionesApplication {
 
+
+
 	public static void main(String[] args) {
-		SpringApplication.run(MsUbicacionesApplication.class, args);
-	}
+    System.setProperty("TNS_ADMIN_PATH", "C:/Users/Usuario/Desktop/prueba3FullStrack/ms-proveedores/src/main/resources/Wallet");
+    SpringApplication.run(MsUbicacionesApplication.class, args);
+}
 
 }
